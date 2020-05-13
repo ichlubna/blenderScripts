e8a55704c3c35d147cd17ba9a51759ff8708277d
ichlubna <ichlubna@fit.vutbr.cz>
2020-05-13 09:27:02 +0200

    Mantaflow fluid bake fix

--- MISC/bakeAll.py
+++ MISC/bakeAll.py
@@ -3,17 +3,12 @@ import bpy
 for scene in bpy.data.scenes:
     for object in scene.objects:
         for modifier in object.modifiers:
-            if modifier.type == 'SMOKE':
-                if modifier.smoke_type == 'DOMAIN':
-                    print("Baking smoke")
-                    override = {'scene': scene, 'active_object': object, 'point_cache': modifier.domain_settings.point_cache}
-                    bpy.ops.ptcache.free_bake(override)
-                    bpy.ops.ptcache.bake(override, bake=True)
-            elif modifier.type == 'FLUID_SIMULATION':
-                if modifier.settings.type == 'DOMAIN':
+            if modifier.type == 'FLUID':
+                if modifier.fluid_type == 'DOMAIN':
                     print("Baking fluid")
-                    override = {'scene': scene, 'active_object': object}
-                    bpy.ops.fluid.bake(override)
+                    object.select_set(True)
+                    bpy.context.view_layer.objects.active = object
+                    bpy.ops.fluid.bake_data()
             elif modifier.type == 'CLOTH':
                 print("Baking cloth")
                 override = {'scene': scene, 'active_object': object, 'point_cache': modifier.point_cache}
